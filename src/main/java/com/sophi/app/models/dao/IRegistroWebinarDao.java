d090f950c5f5f1db2a42555de2f1a98f6fe94372
Dave Moreno <david.moreno@sophitech.mx>
2022-02-18 13:25:10 -0600

    first commit

--- src/main/java/com/sophi/app/models/dao/IRegistroWebinarDao.java
+++
@@ -1,9 +0,0 @@
-package com.sophi.app.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sophi.app.models.entity.RegistroWebinar;
-
-public interface IRegistroWebinarDao extends CrudRepository<RegistroWebinar, Long>{
-
-}
